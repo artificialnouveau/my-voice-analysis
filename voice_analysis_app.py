35ffaa172aff48b46267bd641bcaa54b9228bfd2
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-07-22 07:44:39 +0200

    Update voice_analysis_app.py

--- voice_analysis_app.py
+++ voice_analysis_app.py
@@ -1,87 +1,79 @@
 import os
-import wave
-import requests
+import json
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter.ttk import Progressbar
-from ttkthemes import ThemedStyle
-from customtkinter import CTkButton
+import requests
 from pydub import AudioSegment
+import wave
 import myspsolution as mysp
+
+# Download myspsolution.praat if it doesn't exist
+if not os.path.isfile("myspsolution.praat"):
+    url = "https://raw.githubusercontent.com/Shahabks/my-voice-analysis/master/myspsolution.praat"
+    r = requests.get(url)
+    with open('myspsolution.praat', 'wb') as f:
+        f.write(r.content)
+
 
 class VoiceAnalysisApp(tk.Tk):
     def __init__(self):
-        super().__init__()
-
+        tk.Tk.__init__(self)
         self.title("Voice Analysis App")
-        self.configure(background="#2b2b2b")
-        
-        style = ThemedStyle(self)
-        style.set_theme("black")
+        self.geometry("600x400")
+        self.config(bg="#2b2b2b")
 
-        self.btn_open = CTkButton(master=self, text="Open Audio File", command=self.open_file)
-        self.btn_open.pack(pady=20)
+        self.analyze_button = tk.Button(self, text="Analyze Audio File", command=self.analyze_file)
+        self.analyze_button.pack()
 
-        self.label_info = tk.Label(self, text="Please upload WAV files with a sample rate of 44 kHz and 16 bits of resolution.", bg="#2b2b2b", fg="white")
-        self.label_info.pack(pady=10)
+        self.clear_button = tk.Button(self, text="Clear", command=self.clear_text_area)
+        self.clear_button.pack()
 
-        self.progress = Progressbar(self, orient="horizontal", length=300, mode="indeterminate")
-        
-        self.text_widget = tk.Text(self, fg="white", bg="#2b2b2b")
-        self.text_widget.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
+        self.text_area = tk.Text(self)
+        self.text_area.pack()
 
-        self.download_praat_file()
+    def analyze_file(self):
+        filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                              filetypes=(("Audio files", "*.wav"), ("all files", "*.*")))
+        if filename:
+            try:
+                audio = AudioSegment.from_wav(filename)
+                wav_file = wave.open(filename, 'rb')
+                # Check the requirements
+                if wav_file.getframerate() != 44100 or wav_file.getsampwidth() != 2:
+                    messagebox.showerror("Error", "The audio file must be in *.wav format, recorded at 44 kHz sample frame and 16 bits of resolution.")
+                    return
+                wav_file.close()
 
-    def download_praat_file(self):
-        if not os.path.isfile("myspsolution.praat"):
-            url = "https://raw.githubusercontent.com/Shahabks/my-voice-analysis/master/myspsolution.praat"
-            r = requests.get(url)
-            with open("myspsolution.praat", 'wb') as f:
-                f.write(r.content)
+                # Analyze the audio file
+                self.text_area.insert(tk.END, "Analyzing...\n")
+                self.update()
 
-    def open_file(self):
-        file_path = filedialog.askopenfilename(defaultextension=".wav", filetypes=[("WAV Files", "*.wav")])
-        if file_path:
-            if not self.validate_wav_file(file_path):
-                messagebox.showerror("Error", "The selected file must be a WAV file with a sample rate of 44 kHz and 16 bits of resolution.")
-                return
-            self.analyze_file(file_path)
+                p = filename
+                c = filename.split("/")[-1].split(".")[0]
+                gender = mysp.myspgend(p, c)
+                total = mysp.mysptotal(p, c)
 
-    def validate_wav_file(self, file_path):
-        try:
-            with wave.open(file_path, 'rb') as wf:
-                return wf.getframerate() == 44100 and wf.getsampwidth() == 2
-        except wave.Error:
-            return False
+                results = {
+                    c: {
+                        "Gender recognition": gender,
+                        "Speech mood (semantic analysis)": total['mood'],
+                        "Pronunciation posterior score": total['pronunciation_posterior_score'],
+                        "Articulation-rate": total['articulation_rate'],
+                        "Speech rate": total['speech_rate'],
+                        "Filler words": total['filler_words']
+                    }
+                }
 
-    def analyze_file(self, file_path):
-        self.progress.pack(pady=20)
-        self.progress.start()
-        
-        try:
-            audio = AudioSegment.from_file(file_path)
-            audio.export("temp.wav", format="wav")  # myprosody requires a .wav file
+                with open('results.json', 'w') as f:
+                    json.dump(results, f)
 
-            # Use myspgend function to determine speaker's gender
-            gender = mysp.myspgend("temp", self)
-            
-            # Use mysptotal function for analysis
-            analysis = mysp.mysptotal('temp', gender)
+                self.text_area.insert(tk.END, "Analysis complete.\n")
+            except Exception as e:
+                messagebox.showerror("Error", str(e))
 
-            # Print the analysis result to the text widget
-            self.text_widget.insert("end", f"Speaker's gender: {gender}\n")
-            self.text_widget.insert("end", "Analysis results:\n")
-            for key, value in analysis.items():
-                self.text_widget.insert("end", f"{key.title()}: {value}\n")
+    def clear_text_area(self):
+        self.text_area.delete(1.0, tk.END)
 
-            os.remove("temp.wav")  # remove temporary file
-            
-        except Exception as e:
-            messagebox.showerror("Error", str(e))
-
-        finally:
-            self.progress.stop()
-            self.progress.pack_forget()
 
 if __name__ == "__main__":
     app = VoiceAnalysisApp()
